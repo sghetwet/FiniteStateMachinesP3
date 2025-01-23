119f522d3ec163a0a53001e51da9eb8a8cecd068
Adam Sanchez <145504826+sghetwet@users.noreply.github.com>
2025-01-23 11:02:07 -0800

    did the first twwo
    ya

---
+++ FiniteStatesSanchezP3/Assets/GameEnvironment.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnvironment : MonoBehaviour
+{
+        private static GameEnvironment instance;
+    private List<GameObject> checkpoints = new List<GameObject>();
+    public List<GameObject> Checkpoints { get { return checkpoints; } }
+
+    public static GameEnvironment Singleton
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameEnvironment();
+                instance.Checkpoints.AddRange(
+                    GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+                return instance;
+        }
+    }
+}
+
+
+    // Update is called once per frame
+
